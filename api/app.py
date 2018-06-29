dc04182b57df9195753c0723804ccede8a74b418
njonge10 <andrew.waruiru22@gmail.com>
2018-06-28 20:57:22 -0700

    Updated

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ from flask import redirect
 from flask import url_for
 from flask import request
 from flask import jsonify
+from flask import make_response
 import json
 
 app = Flask(__name__)
@@ -26,13 +27,18 @@ def register():
     name = data['name']
     email = data['email']
     password = data['password']
+    data = request.get_json()
+    return make_response(jsonify({"name":name,"email":email,"password":password,"message":"Registration Successful"}))
 
 @app.route('/api/v1/offer',methods=['POST'])
 def offer_a_ride():
     depart_location = data['depart_location']
-    destination_location = data['depart_location']
+    destination_location = data['destination_location']
     fare = data['fare']
     route = data['route']
+    driver = data['driver']
+    return make_response(jsonify({"depart_location":depart_location,
+                      "destination_location":destination_location,"fare":fare,"route":route}))
 
 @app.route('/api/v1/request',methods=['GET'])
 def request_a_ride():
@@ -41,6 +47,8 @@ def request_a_ride():
     depart_location = data['depart_location']
     destination_location = data['depart_location']
     driver = data['driver']
+    return make_response(jsonify({"route":route,"fare":fare,"depart_location":depart_location,
+                                  "destination_location":destination_location,"driver":driver}))
 
 @app.route('/api/v1/total_rides',methods=['GET'])
 def total_rides():
@@ -49,11 +57,14 @@ def total_rides():
     depart_location = data['depart_location']
     destination_location = data['depart_location']
     driver = data['driver']
+    return make_response(jsonify({"route":route,"fare":fare,"depart_location":depart_location,
+                                  "destination_location":destination_location,"driver":driver}))
 
 @app.route('/api/v1/admin',methods=['GET'])
 def admin():
     name = data['name']
     password = data['password']
+    return make_response(jsonify({"name":name,"password":password }))
 
 
 if __name__ == '__main__':
